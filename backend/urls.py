11891dd865433f490b8d028b3ab2364646e93ada
Jerodsun <Jerod.sun@live.com>
2019-08-04 22:29:36 -0700

    register urls

--- backend/urls.py
+++ backend/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+
+from randomwalk import views
+
+router = routers.DefaultRouter()
+router.register(r'randomwalk', views.SampleDataView, 'randomwalk')
+
+# I believe we can register multiple routers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
