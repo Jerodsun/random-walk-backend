123ff9377ecf3258e62d947bbfbb437e33766cbb
Jerodsun <Jerod.sun@live.com>
2019-08-05 20:28:32 -0700

    create blackscholes view

--- randomwalk/views.py
+++ randomwalk/views.py
@@ -1,19 +1,19 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from .models import SampleData
-from .serializers import InputSerializer
+from .models import SampleData, BlackScholes
+from .serializers import SampleInputSerializer, BlackScholesSerializer
 
 # Create your views here.
 
 class SampleDataView(viewsets.ModelViewSet):
     """ This shows up in the title. """
-    serializer_class = InputSerializer
+    serializer_class = SampleInputSerializer
     queryset = SampleData.objects.all()
     http_method_names = ['get', 'post']
 
     def create(self, request):
-        serializer = InputSerializer(data=request.data)
+        serializer = SampleInputSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(ip_address=self.get_client_ip(request))
             return Response({'message':'success', 'params':serializer.validated_data})
@@ -27,3 +27,9 @@ class SampleDataView(viewsets.ModelViewSet):
         else:
             ip = request.META.get('REMOTE_ADDR')
         return ip
+
+class BlackScholesView(viewsets.ModelViewSet):
+    
+    serializer_class = BlackScholesSerializer
+    queryset = BlackScholes.objects.all()
+    http_method_names = ['get', 'post']
