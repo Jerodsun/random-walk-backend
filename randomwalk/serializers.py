6be56e3877d6824c2d5b59478efd50560d061022
Jerodsun <Jerod.sun@live.com>
2019-08-05 20:28:03 -0700

    create blackscholes serializer

--- randomwalk/serializers.py
+++ randomwalk/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
 
-from .models import SampleData
+from .models import SampleData, BlackScholes
 
-class InputSerializer(serializers.ModelSerializer):
+class SampleInputSerializer(serializers.ModelSerializer):
     """ The InputSerializer exposes which fields are required to be POSTed """
     class Meta:
         model = SampleData
         fields = ['id', 'created', 'direction']
+
+class BlackScholesSerializer(serializers.ModelSerializer):
+    """ Need a better description for what a serializer does """
+    # https://www.django-rest-framework.org/tutorial/1-serialization/
+    id = serializers.IntegerField(read_only=True)
+    price = serializers.FloatField()
+    strike = serializers.DecimalField(max_digits=6, decimal_places=1)
+    interest_rate = serializers.FloatField()
+    volatility = serializers.FloatField()
+    time_to_exp = serializers.IntegerField()
+
+    class Meta:
+        model = BlackScholes
+        fields = ['id', 'created', 'price', 'strike', 'interest_rate', 'volatility', 'time_to_exp']
+
+    def create(self, validated_data):
+        return BlackScholes.objects.create(**validated_data)
