8de4079c384234b52ee03f2b8e746b4e343b5df0
Jerodsun <Jerod.sun@live.com>
2019-08-06 18:49:33 -0700

    add django admin panel

--- randomwalk/admin.py
+++ randomwalk/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import SampleData, BlackScholes
 
 # Register your models here.
+
+class SampleDataAdmin(admin.ModelAdmin):
+    list_display = ('created', 'direction', 'ip_address')
+
+class BlackScholesAdmin(admin.ModelAdmin):
+    list_display = ('created', 'price', 'strike', 'interest_rate', 'volatility', 'time_to_exp')
+
+admin.site.register(SampleData, SampleDataAdmin)
+admin.site.register(BlackScholes, BlackScholesAdmin)
