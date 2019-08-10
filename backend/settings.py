5a0b377c7d8a14df42de2b913a39b69daa007036
Jerodsun <Jerod.sun@live.com>
2019-08-10 11:22:58 -0700

    add cors headers

--- backend/settings.py
+++ backend/settings.py
@@ -41,12 +41,13 @@ INSTALLED_APPS = [
     'randomwalk',
     'rest_framework',
     'rest_framework_swagger',
-    
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,5 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Cors Headers
+# https://github.com/ottoyiu/django-cors-headers
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = [] # Allow all for now
 
 django_heroku.settings(locals())
